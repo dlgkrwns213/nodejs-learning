123b33f4cd1084c532e39e0e44ed8792868a4e0c
이학준 <dlgkrwns213@naver.com>
2025-01-04 01:14:10 +0900

    express setting

---
+++ 06. express/myContacts/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Node!");
+});
+
+app.get("/contacts", (req, res) => {
+  res.send("Contacts Page");
+});
+
+app.listen(3000, () => {
+  console.log('서버 실행 중');
+});
