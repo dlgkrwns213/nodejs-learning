ab475790a672b24ae26ec8005e60f9d2ca12576c
이학준 <dlgkrwns213@naver.com>
2025-01-14 01:42:39 +0900

    Change login function to use JWT for authentication

--- 11. Project1_1 - Login/controllers/loginController.js
+++ 11. Project1_1 - Login/controllers/loginController.js
@@ -1,6 +1,11 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+const jwtSecret = process.env.jwt_Secret;
 
 // Get login page
 // Get /
@@ -12,12 +17,19 @@ const getLogin = (req, res) => {
 // POST /
 const loginUser = asyncHandler( async(req, res) => {
   const {username, password} = req.body;
-
-  if (username === "admin" && password == "1234") {
-    res.send("Login success");
-  } else {
-    res.send("Login Failed");
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.json({ message: "일치하는 사용자가 없습니다." });
   }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.json({ message: "올바르지 않은 비밀번호 입니다." });
+  }
+
+  const token = jwt.sign({ id: user._id }, jwtSecret);
+  res.cookie("token", token, {httpOnly: true});
+  res.redirect("/contacts");
 });
 
 // Register page
