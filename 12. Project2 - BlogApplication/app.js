d3ef2e2f8cb6404b07444362554225f1a9d52204
이학준 <dlgkrwns213@naver.com>
2025-01-15 17:34:38 +0900

    Register admin

--- 12. Project2 - BlogApplication/app.js
+++ 12. Project2 - BlogApplication/app.js
@@ -18,6 +18,9 @@ app.set("views", "./views");
 
 app.use(express.static("public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", routerMain);
 app.use("/", routerAdmin);
 
--- 12. Project2 - BlogApplication/routes/admin.js
+++ 12. Project2 - BlogApplication/routes/admin.js
@@ -1,4 +1,7 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -11,5 +14,23 @@ router.get("/admin", (req, res) => {
   };
   res.render("admin/index", {locals, layout: "../views/layouts/admin-nologout.ejs"});
 });
+
+// View Register Form
+// GET /register
+router.get("/register", asyncHandler( async (req, res) => {
+  res.render("admin/index", {layout: "../views/layouts/admin-nologout.ejs"});
+}));
+
+// View Register Form
+// POST /register
+router.post("/register", asyncHandler( async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const user = await User.create({
+    username: req.body.username,
+    password: hashedPassword
+  });
+
+  res.json(`user created: ${user}`);
+}));
 
 export default router;
